cb7eeb3771cdf59b0007fd6e23310bf33eb68115
MP242 <marc.pollet@epitech.eu>
2023-04-05 22:19:54 +0400

    add jwt and auth

--- expressApp/controllers/userCtrl.js
+++ expressApp/controllers/userCtrl.js
@@ -2,11 +2,14 @@ const User = require('../models/user.model');
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const {generateToken,verifyToken} = require('../auth/jwt');
 
 exports.allUsers = async function(req, res, next) {
   try {
     const users = await User.find();
-    res.json(users);
+    res.status(200).json(users);
   } catch (err) {
     console.error(err);
     res.status(500).send('Error retrieving users from database');
@@ -36,7 +39,7 @@ exports.createUser = async function(req, res, next) {
     });
 
     await newUser.save();
-    res.json(newUser);
+    res.status(200).json(newUser);
   } catch (err) {
     console.error(err);
     res.status(500).send('Error inserting user into database');
@@ -46,7 +49,7 @@ exports.createUser = async function(req, res, next) {
 exports.getOneUser = async function(req, res, next) {
   try {
     const user = await User.findById(req.params.id);
-    res.json(user);
+    res.status(200).json(user);
   } catch (err) {
     console.error(err);
     res.status(500).send('Error retrieving user from database');
@@ -78,7 +81,7 @@ exports.updateUser = async function(req, res, next) {
 
     user.updated_at = new Date();
     const updatedUser = await user.save();
-    res.json({ message: `User with id ${updatedUser._id} updated` });
+    res.status(200).json({ message: `User with id ${updatedUser._id} updated` });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error updating user' });
@@ -92,9 +95,31 @@ exports.deleteUser = async function(req, res, next) {
       return res.status(404).send('User not found');
     }
     console.log(`Deleted user with id ${req.params.id}`);
-    res.status(200).send(`User with id ${req.params.id} deleted`);
+    res.status(200).json({"message":`User with id ${req.params.id} deleted`});
   } catch (err) {
     console.error(err);
     res.status(500).send('Error deleting user from database');
   }
+}
+
+exports.login = async function(req, res, next) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  // Vérifier que l'utilisateur existe
+  const user = await User.findOne({ email });
+  if (!user) {
+      return res.status(400).send('Invalid email! or password');
+  }
+  // Vérifier le mot de passe
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+      return res.status(400).send('Invalid email or password!');
+  }
+  // Générer un token et le renvoyer
+  // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+  const token = generateToken(user);
+  res.status(200).json({'token': token });
 }
